f73de22db4bd5dcfa2c01053e39db47cae8c0ad8
kbatnij <batnij.k@gmail.com>
2024-03-21 03:05:05 -0700

    add batcat config

--- mint-linux-setup/setup-scripts/bat_cat.sh
+++ mint-linux-setup/setup-scripts/bat_cat.sh
@@ -9,7 +9,6 @@ install_bat() {
 
     echo "Attempting to install bat..."
     if sudo apt-get update && sudo apt-get install -y bat; then
-        mkdir -p ~/.local/bin
         sudo ln -s /usr/bin/batcat /usr/bin/bat
         echo "bat installed successfully."
     else
@@ -18,8 +17,30 @@ install_bat() {
     fi
 }
 
+setup_bat_config_symlink() {
+    echo "Setting up bat configuration symlink..."
+    local custom_config_path="$PWD/.config/bat/config"
+    local default_config_path="$HOME/.config/bat/config"
+
+    mkdir -p "$HOME/.config/bat"
+
+    if [[ -L "$default_config_path" ]] || [[ -f "$default_config_path" ]]; then
+        echo "A bat configuration or symlink already exists. Removing it..."
+        rm -f "$default_config_path"
+    fi
+
+    if [[ -f "$custom_config_path" ]]; then
+        ln -s "$custom_config_path" "$default_config_path"
+        echo "bat configuration successfully symlinked."
+    else
+        echo "Custom bat configuration file not found. Please check the path."
+        exit 1
+    fi
+}
+
 main() {
     install_bat
+    setup_bat_config_symlink
 }
 
 main
